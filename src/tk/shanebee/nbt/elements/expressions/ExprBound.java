9273f459276d0252ca5c62216ff7f0b8131354c8
ShaneBeee <shanebolenback@me.com>
2019-10-06 13:24:13 -0700

    Bound - changes - Add syntax to create a "full" bound (from y=0 to y=255)

--- src/tk/shanebee/nbt/elements/expressions/ExprBound.java
+++ src/tk/shanebee/nbt/elements/expressions/ExprBound.java
@@ -12,33 +12,47 @@ import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.event.Event;
 import tk.shanebee.nbt.elements.objects.Bound;
 
 @Name("Bounding Box")
-@Description("Create a new 3D bounding box between 2 points. This could be useful for creating your own region system.")
-@Examples("set {bound} to bound between {loc1} and {loc2}")
+@Description({"Create a new 3D bounding box between 2 points. This could be useful for creating your own region system. ",
+        "Optional \"full\" bound is a bound that extends from Y=0 to Y=255 (of whatever max height your world is)."})
+@Examples({"set {bound::1} to bound between {loc1} and {loc2}",
+        "set {bound::2} to full bound between {loc1} and {loc2}"})
 @Since("2.6.0")
 public class ExprBound extends SimpleExpression<Bound> {
 
     static {
         Skript.registerExpression(ExprBound.class, Bound.class, ExpressionType.SIMPLE,
-                "[a] [new] bound between %location% and %location%");
+                "[a] [new] [(1¦full)] bound between %location% and %location%");
     }
 
     private Expression<Location> lesser, greater;
+    private boolean full;
 
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] expressions, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
         this.lesser = (Expression<Location>) expressions[0];
         this.greater = (Expression<Location>) expressions[1];
+        this.full = parseResult.mark == 1;
         return true;
     }
 
     @Override
     protected Bound[] get(Event event) {
-        return CollectionUtils.array(new Bound(lesser.getSingle(event), greater.getSingle(event)));
+        Location lesser = this.lesser.getSingle(event);
+        Location greater = this.greater.getSingle(event);
+        World w = greater.getWorld();
+        assert w != null;
+        int max = w.getMaxHeight();
+        if (full) {
+            lesser.setY(0);
+            greater.setY(max);
+        }
+        return CollectionUtils.array(new Bound(lesser, greater));
     }
 
     @Override
